4f1849e68f26ad891351c1f3d2d14614bc95f9c6
Mike Taylor <mikecolintaylor@gmail.com>
2015-08-19 16:44:07 +0100

    course project done

--- regression models quizzes.R
+++ regression models quizzes.R
@@ -49,9 +49,9 @@ x <- c(0.61, 0.93, 0.83, 0.35, 0.54, 0.16, 0.91, 0.62, 0.62)
 y <- c(0.67, 0.84, 0.6, 0.18, 0.85, 0.47, 1.1, 0.65, 0.36)
 summary(lm(y ~ x))
 
-
-
-
+yx <- lm(y ~ x)
+sd(yx$residuals)
+sqrt(deviance(yx) / df.residual(yx))
 
 mpgPred <- lm(mpg ~ wt, data = mtcars)
 
@@ -63,13 +63,39 @@ predict(mpgPred, data.frame(wt = 3), interval = "prediction")
 
 confint(mpgPred) * 2
 
-#q3
+#It would get multiplied by 100.
+#The new intercept would be ??^0???c??^1
+
+
+sum(lm(mpg ~ 1, data = mtcars)$residuals) / sum(mpgPred$residuals)
+#0.25
+#If an intercept is included, then they will sum to 0.
+
+#quiz 3
 
 mtcars$cyl <- as.factor(mtcars$cyl)
 mpgPred2 <- lm(mpg ~ cyl + wt, data = mtcars)
 summary(mpgPred2)
 mpgPred3 <- lm(mpg ~ cyl, data = mtcars)
 summary(mpgPred3)
+#Holding weight constant, cylinder appears to have less of an impact on mpg than if weight is disregarded.
 
 mpgPred4 <- lm(mpg ~ cyl * wt, data = mtcars)
 summary(mpgPred4)
+
+anova(mpgPred2, mpgPred4)
+#The P-value is larger than 0.05. So, according to our criterion, we would fail to reject, which suggests that the interaction terms may not be necessary.
+
+#question 4
+#The estimated expected change in MPG per half ton increase in weight.
+
+x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
+y <- c(0.549, -0.026, -0.127, -0.751, 1.344)
+hatvalues(lm(y ~ x))
+
+which(hatvalues(lm(y ~ x)) == max(hatvalues(lm(y ~ x))))
+dfbetas(lm(y ~ x))[5, ]
+
+#question 7
+#It is possible for the coefficient to reverse sign after adjustment. For example, it can be strongly significant and positive before adjustment and strongly significant and negative after adjustment.
+
